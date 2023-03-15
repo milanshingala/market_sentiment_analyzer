72e666d692ea1a117794ddff8c9a0b0e8cdd98b9
U6071047 <milanshingala666@gmail.com>
2023-03-15 17:53:08 +0530

    application changes

--- trst.py
+++ trst.py
@@ -1,10 +1,10 @@
-import datetime
-Current_Date=str(datetime.datetime.today())
-print(Current_Date)
-list=Current_Date.split(' ')
-print(list[0])
-
-
-
-
-
+# import datetime
+# Current_Date=str(datetime.datetime.today())
+# print(Current_Date)
+# list=Current_Date.split(' ')
+# print(list[0])
+#
+#
+#
+#
+#
